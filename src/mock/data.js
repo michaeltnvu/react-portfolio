58e983342fcc3844bbfecb309070b9f3347fe3a6
Tien (Michael) Vu <69743095+michaeltnvu@users.noreply.github.com>
2021-10-06 16:24:10 -0400

    Updated resume link

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: "I graduated from San Jose State University in 2017 and became an accountant in the heart of Silicon Valley. In 2019, my fiancée and I relocated to South Florida so that she could attend medical school. Although I excelled in accounting and finance, I decided to pursue a different career following the move across the country.",
   paragraphTwo: "I completed Thinkful's Software Engineer bootcamp, and I am now seeking a new role as a Full-Stack Developer. My goal is to continuously grow as a developer while staying current with the latest trends and technologies. I want to work for a passionate team that gets excited about their work and is not afraid to push boundaries.",
   paragraphThree: "Tools I use: HTML5, CSS3, JavaScript, jQuery, React, Gatsby.js, Styled Components, Git, Node.js, Express, and PostgreSQL.",
-  resume: 'https://docs.google.com/document/d/1AriRipcL6zxMwUjDRtIL9sEryuE1uKadrOANFQwv2d0/edit?usp=sharing', // if no resume, the button will not show up
+  resume: 'https://docs.google.com/document/d/1Ib9_MVrKVBopGUR_Ixq8jn_eBywxBq-8iQo0tKJf83Q/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
